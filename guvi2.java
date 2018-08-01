bb9312720c313ff169d38fa5602e5316ad628803
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-01 11:31:03 +0530

    Create guvi2.java

---
+++ guvi2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class guvi2
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+          if(a<0)
+        {
+            System.out.print("Invalid");
+        }
+        if(a>0)
+        {
+        if(a%2==0)
+        {
+            System.out.print("EVEN");
+        }
+        else
+        {
+            System.out.print("odd");
+        }
+    }
+    }
+}
+
