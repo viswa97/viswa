2c33463940fb79b77d710d94b65e28c8080e6ba1
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-01 11:55:28 +0530

    Create guvi3.java

---
+++ guvi3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class guvi3
+{
+public static void main(String arg[])
+{
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    if(s=='a'&&s=='e'&&s=='i'&&s=='o'&&s=='u'||s=='A'&&s=='E'&&s=='I'&&s=='O'&&s=='U')
+    {
+        System.out.print("vowel");
+    }
+    else
+    {
+        System.out.print("consonant");
+    }
+    }
+}
+
+
+
+
+
