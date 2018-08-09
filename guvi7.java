f08bd7a84b415550e8beeaf57801d315174795bb
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-09 15:19:42 +0530

    Create guvi7.java

---
+++ guvi7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class guvi7
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            System.out.println("hello");
+        }
+    }
+}
+        
+  
+
