b5556b50a6bb66613be4549a0d3120de07f55609
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-09 16:17:45 +0530

    Create guvi11.java

---
+++ guvi11.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+class guvi11
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(Math.pow(a,b));
+    }
+}
+        
+  
+
