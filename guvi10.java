8886740a30a31e2af16d1357d5fd3d11169b93f3
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-09 15:57:29 +0530

    Create guvi10.java

---
+++ guvi10.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class guvi10
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.print(a.length());
+    }
+}
+        
+  
+
