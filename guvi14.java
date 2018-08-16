c717b9956a38597c469b552165e1dbfd52539b35
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-16 15:52:36 +0530

    Create guvi14.java

---
+++ guvi14.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class guvi14
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(i%2!=0)
+            {
+            System.out.print(i+" ");
+            }
+        }
+    }
+}
