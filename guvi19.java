f171800255d0335aa6ad136bd355cf6e375cb1e3
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-24 12:06:51 +0530

    Create guvi19.java

---
+++ guvi19.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.*; 
+class guvi19
+{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=1;
+    for(int i=1;i<=a;i++)
+    {
+        b=b*i;
+    }
+    System.out.print(b);
+}
+}
+    
