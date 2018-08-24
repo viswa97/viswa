ae18944940836afc665957ff8654eb2e02319d3b
viswa97 <41982225+viswa97@users.noreply.github.com>
2018-08-24 15:04:07 +0530

    Update guvi22.java

--- guvi22.java
+++ guvi22.java
@@ -7,19 +7,13 @@ class guvi22
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int a[]=new int[n];
-        int max;
         for(int i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
         }
-        max=a[0];
-        for( int i=0;i<n;i++)
-        {
-            if(max<a[i])
-            {
-                max=a[i];
-            }
-        }
-            System.out.print(max);
+        Arrays.sort(a);
+        System.out.print(a[a.length-1]);
     }
 }
+
+
